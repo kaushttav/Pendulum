8bb32f38ead79073b991886d960395737cf52089
kaushttav <59498306+kaushttav@users.noreply.github.com>
2020-03-20 21:28:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,68 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-var pendulum;
-var pendulum_image;
-var wall1;
-var wall2;
-var string;
-var holder;
+var engine, world;
+var ball,ground;
+var ball_image;
 
 
-function preload(){
-  pendulum_image= loadImage("pendulum.png");
-}
 function setup() {
-  createCanvas(800,400);
-  pendulum = createSprite(400, 300, 50, 50);
-  pendulum.addImage(pendulum_image)
-  pendulum_image.resize(35,35);
-  pendulum.velocityX = 5;
-  wall1 = createSprite(250,10,20,800);
-  wall1.visible = false;
-  wall2 = createSprite(550,10,20,800);
-  wall2.visible = false;
-  
-  string = createSprite(200,140,1,290);
-  string.shapeColor = "white";
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options={
+    isStatic : true
+  }
+
+  ground = Bodies.rectangle(200,330,400,20,ground_options)
+  World.add(world,ground);
+
+
+var ball_options = {
+
+  restitution : 1.0,
+  density : 1.0
+
 }
+
+ball  = Bodies.circle(220,200,40,ball_options);
+
+World.add(world,ball);
+
+
+var options = {
+  bodyA : ball,
+  bodyB : ball,
+  stiffness: 0.004,
+  length : 100
+}
+var string = Constraint.create(options);
+World.add(world,string);
+
+fill("WHite");
+}
+
 
 function draw() {
-  background("black");  
-  pendulum.bounceOff(wall1);
-  pendulum.bounceOff(wall2);
-  string.x = pendulum.x ;
-  
-  drawSprites();
+  background(0); 
+  Engine.update(engine);
+
+fill(0);
+rectMode(CENTER);
+rect(ground.position.x,ground.position.y,400,20);
+
+
+
+fill("blue");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,40);
+
+strokeWeight(8);
+stroke("white");
+line(200,5,ball.position.x,ball.position.y);
+ball.position.x = mouseX;
+ball.position.y = mouseY;
 }
